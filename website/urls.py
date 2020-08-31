878b4a36208e521c1a4ac50b0b6807e61cd803bc
jadielmwangi <jedielmwangi@gmail.com>
2020-08-31 13:58:31 +0300

    adds appointment functionality

--- website/urls.py
+++ website/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('pricing.html', views.pricing, name="pricing"),
     path('blog.html', views.blog, name="blog"),
 
-    path('blog_details.html', views.blog_details, name=" blog_details "),  
+    path('blog_details.html', views.blog_details, name=" blog_details "),
+    path('appointment.html', views.appointment, name="appointment"),
+
+
 ]
--- website/views.py
+++ website/views.py
@@ -20,6 +20,9 @@ def blog(request):
 def blog_details(request):
     return render(request, 'blog_details.html' , {})
 
+def appointment(request):
+    return render(request, 'appointment.html' , {})
+
 
 
 def contact(request):
@@ -31,10 +34,11 @@ def contact(request):
 
         # send an email
         send_mail(
-            'Message from:' +  message_name, # subject
-            'The message is:' + message, # message
-            message_email, # from email
-            ['jedielmwangi@gmail.com'], # to Email
+            message_name , # subject
+            message , # message
+            message_email , # from email
+            
+            [ 'jedielmwangi@gmail.com' ]  # to Email
 
         )
 
@@ -45,3 +49,50 @@ def contact(request):
 
         return render(request, 'contact.html', {})
 
+
+
+
+
+
+
+
+
+
+
+def appointment(request):
+    if request.method == "POST":
+        your_name = request.POST['your-name']
+        your_phone  = request.POST['your-phone']
+        your_email  = request.POST['your-email']
+        your_address  = request.POST['your-address']
+        your_schedule  = request.POST['your-schedule']
+        your_date  = request.POST['your-date']
+        your_message  = request.POST['your-message']
+
+        # m 
+
+        # send an email
+        appointment = "Name:" + your_name +  " /n Mobile no:" + your_phone + "  /n Emaail address:" + your_email + "  /n Day:" + your_date + "  /n Day:" + your_date + "  /n Message:" + your_message + "."
+        send_mail(
+            ' Appointment request', # subject
+            'appointment', # message
+            'your_email' , # from email
+            ['jedielmwangi@gmail.com'], # to Email
+
+        )
+
+        return render(request, 'appointment.html', { 
+            
+            'your_name' : your_name,
+            'your_phone' :  your_phone,
+            'your_email' :  your_email,
+            'your_schedule' :  your_schedule,
+            'your_address' : your_address,
+            'your_message' : your_message,
+            'your_date ' : your_date,
+            })
+
+
+    else: 
+
+        return render(request, 'home.html', {})
